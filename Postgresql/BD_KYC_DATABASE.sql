22a5c32b72150d7b3f36b5681825aca56e6971b8
LHPH <luislyly@hotmail.com>
2024-01-23 17:54:47 -0600

    Adjust in SQL files

--- Postgresql/BD_KYC_DATABASE.sql
+++ Postgresql/BD_KYC_DATABASE.sql
@@ -3,6 +3,8 @@ CREATE DATABASE kyc_database;
 
 CREATE USER kyc_user WITH ENCRYPTED PASSWORD 'kyc_pass';
 
-GRANT ALL PRIVILEGES ON DATABASE kyc_database TO kyd_user;
+ALTER USER kyc_user OWNER TO kyc_database;
+
+GRANT ALL PRIVILEGES ON DATABASE kyc_database TO kyc_user;
 
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
